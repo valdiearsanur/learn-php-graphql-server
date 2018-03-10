13899cd543d2eb085f34fc974d398832732a6dd9
Muhammad Valdie Arsanur <valdiearsanur@gmail.com>
2018-03-10 20:30:01 +0700

    continue setup DI

---
+++ workspace/project/Project/DependencyInjection/DIResolver.php
@@ -0,0 +1,35 @@
+<?php
+namespace Project\DependencyInjection;
+
+use DI\Container;
+
+class DIResolver {
+	private static $container;
+	
+	public static function has($name)
+	{
+		$result = static::getContainer()->has($name);
+		return $result;
+	}
+
+	public static function resolve($name)
+	{
+		$class = static::getContainer()->get($name);
+		return $class;
+	}
+
+	public static function resolveNewInstance($name)
+	{
+		$class = static::getContainer()->make($name);
+		return $class;
+	}
+
+	private static function getContainer()
+	{
+		if (static::$container === null) {
+			global $container;
+			static::$container = $container;
+		}
+		return static::$container;
+	}
+}
