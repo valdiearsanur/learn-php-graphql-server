57053e7172b5f505e3e49b41c7b243c69620398f
Muhammad Valdie Arsanur <valdiearsanur@gmail.com>
2018-03-10 20:02:04 +0700

    setup slim

--- workspace/project/index.php
+++ workspace/project/index.php
@@ -1,2 +1,13 @@
 <?php
-phpinfo();
+
+require __DIR__ . '/vendor/autoload.php';
+
+// initiate app
+$app = new \Slim\App;
+
+$app->get('/', function () {
+	echo 'hello from SLIM Framework';
+});
+
+$app->run();
+?>
